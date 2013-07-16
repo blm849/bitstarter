58dd66788e8a60383c3a385bc862ddffd6d5593c
Bernie Michalik <bernie_michalik@yahoo.com>
2013-07-16 04:23:41 +0000

    adding latest index.html/web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var fs = require("fs"); 
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+   var temp = fs.readFileSync('index.html', 'utf8');
+   response.send(temp);
 });
 
 var port = process.env.PORT || 5000;
